a20ea630928c2c4de93757bad15d114c631002a6
khanhnhanbk <nhanhoa21012002@gmail.com>
2023-01-13 15:43:36 +0700

    navbar active

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,3 +1,8 @@
+<?php
+$page = basename($_SERVER['PHP_SELF']);
+
+?>
+
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
@@ -9,7 +14,7 @@
     <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link text-white " href="index.php">
+                <a class="nav-link text-white <?=$page == "index.php" ? "active bg-gradient-primary":"";?>" href="index.php">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="material-icons opacity-10">dashboard</i>
                     </div>
@@ -18,7 +23,7 @@
             </li>
             <!-- category -->
             <li class="nav-item">
-                <a class="nav-link text-white " href="all-categories.php">
+                <a class="nav-link text-white <?=$page == "all-categories.php" ? "active bg-gradient-primary":"";?> " href="all-categories.php">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="material-icons opacity-10">table_view</i>
                     </div>
@@ -26,16 +31,16 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white " href="add-category.php">
+                <a class="nav-link text-white <?=$page == "add-category.php" ? "active bg-gradient-primary":"";?>" href="add-category.php">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="material-icons opacity-10">table_view</i>
+                        <i class="material-icons opacity-10">add</i>
                     </div>
                     <span class="nav-link-text ms-1">Add new category</span>
                 </a>
             </li>
               <!-- product -->
               <li class="nav-item">
-                <a class="nav-link text-white " href="all-products.php">
+                <a class="nav-link text-white <?=$page == "all-products.php" ? "active bg-gradient-primary":"";?> " href="all-products.php">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="material-icons opacity-10">table_view</i>
                     </div>
@@ -43,9 +48,9 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white " href="add-product.php">
+                <a class="nav-link text-white <?=$page == "add-product.php" ? "active bg-gradient-primary":"";?>" href="add-product.php">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="material-icons opacity-10">table_view</i>
+                        <i class="material-icons opacity-10">add</i>
                     </div>
                     <span class="nav-link-text ms-1">Add new product</span>
                 </a>
